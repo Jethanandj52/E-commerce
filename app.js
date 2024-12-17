f435686f352b697b0d548bd5c85864fe973c22a8
Jethanandj52 <134337163+Jethanandj52@users.noreply.github.com>
2024-12-18 03:16:07 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 let cards=[
-    {id:1,name:"Grocery",price:10,discription:"Grocery includes everyday food items, vegetables, fruits, and household essentials for daily needs.",Image:"images/Grossery.jpg"},
-    {id:2,name:"Computer Hardware",price:20,discription:"Computer hardware refers to the physical parts of a computer, like CPU, RAM, motherboard, and peripherals.", Image:"images/computer hardware.jpg"},
-    {id:3,name:"Medicine",price:30,discription:"Medicine refers to drugs or treatments used to prevent, cure, or manage diseases and improve health.", Image:"images/medicine store.jpg"},
-    {id:4,name:"Clothes",price:40,discription:"Clothes are garments made from fabrics, worn to cover, protect, and style the body daily. And looking Good.", Image:"images/clothes.jpg"},
+    {id:1,name:"Grocery",price:10,discription:"Grocery includes everyday food items, vegetables, fruits, and household essentials for daily needs.",Image:"Grossery.jpg"},
+    {id:2,name:"Computer Hardware",price:20,discription:"Computer hardware refers to the physical parts of a computer, like CPU, RAM, motherboard, and peripherals.", Image:"computer hardware.jpg"},
+    {id:3,name:"Medicine",price:30,discription:"Medicine refers to drugs or treatments used to prevent, cure, or manage diseases and improve health.", Image:"medicine store.jpg"},
+    {id:4,name:"Clothes",price:40,discription:"Clothes are garments made from fabrics, worn to cover, protect, and style the body daily. And looking Good.", Image:"clothes.jpg"},
   
 ]
 
